189489063538e30f05dfe44621c0112d6b87fe06
NataliePro <chensy186@gmail.com>
2018-10-02 10:14:53 +0300

    HW_0_v2

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -14,41 +14,45 @@ import static ru.javawebinar.topjava.util.TimeUtil.isBetween;
 
 public class UserMealsUtil {
 
-    private static Map<LocalDate, Integer> calPerDayMap = new HashMap<>();
-
     public static void main(String[] args) {
         List<UserMeal> mealList = Arrays.asList(
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
-        getFilteredWithExceededByCycle(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        getFilteredWithExceededByStream(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
     }
 
     public static List<UserMealWithExceed> getFilteredWithExceededByCycle(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        setCalPerDayMap(mealList);
         List<UserMealWithExceed> filteredList = new ArrayList<>();
         mealList.forEach(userMeal -> {
-            if (isBetween(userMeal.getTime(),startTime,endTime)){
-                filteredList.add(new UserMealWithExceed(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), calPerDayMap.getOrDefault(userMeal.getDate(),-1) > caloriesPerDay));
+            if (isBetween(userMeal.getTime(), startTime, endTime)) {
+                filteredList.add(new UserMealWithExceed(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), getCalPerDayMap(mealList).get(userMeal.getDate()) > caloriesPerDay));
             }
         });
         return filteredList;
     }
 
     public static List<UserMealWithExceed> getFilteredWithExceededByStream(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        setCalPerDayMap(mealList);
+        Map<LocalDate, Integer> calPerDayMap = new HashMap<>();
         return mealList.stream()
+                .map(userMeal -> {
+                    calPerDayMap.merge(userMeal.getDate(), userMeal.getCalories(), Integer::sum);
+                    return userMeal;
+                })
+                .collect(Collectors.toList())
+                .stream()
                 .filter(userMeal -> isBetween(userMeal.getTime(), startTime, endTime))
-                .map(userMeal -> new UserMealWithExceed(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), calPerDayMap.getOrDefault(userMeal.getDate(),-1) > caloriesPerDay))
+                .map(userMeal -> new UserMealWithExceed(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), calPerDayMap.get(userMeal.getDate()) > caloriesPerDay))
                 .collect(Collectors.toList());
     }
 
-    public static void setCalPerDayMap(List<UserMeal> mealList) {
-        calPerDayMap.clear();
-        mealList.forEach(userMeal -> calPerDayMap.merge(userMeal.getDate(), userMeal.getCalories(), (oldVal, newVal) -> oldVal + newVal));
+    public static Map<LocalDate, Integer> getCalPerDayMap(List<UserMeal> mealList) {
+        Map<LocalDate, Integer> calPerDayMap = new HashMap<>();
+        mealList.forEach(userMeal -> calPerDayMap.merge(userMeal.getDate(), userMeal.getCalories(), Integer::sum));
+        return calPerDayMap;
     }
 }
